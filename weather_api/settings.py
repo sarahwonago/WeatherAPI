2bd016d8103f39591de07df6d0c6802ac7233361
sarahwonago <sarahshaaleh@gmail.com>
2024-10-03 15:30:38 +0300

    initial project setup

--- weather_api/settings.py
+++ weather_api/settings.py
@@ -120,3 +120,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# weather api details
+WEATHER_API_KEY = config('WEATHER_API_KEY')
+WEATHER_API_URL = config('WEATHER_API_URL')
